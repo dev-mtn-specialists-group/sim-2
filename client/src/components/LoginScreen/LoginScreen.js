64064bc1e3bc5ee1801f57eb4c8bbbdda12b8d37
Pack <apack@UTMPG04820.local>
2018-02-10 10:40:05 -0700

    Fixed the input types

--- client/src/components/LoginScreen/LoginScreen.js
+++ client/src/components/LoginScreen/LoginScreen.js
@@ -11,9 +11,9 @@ export default class LoginScreen extends Component {
       <div class="Auth__container white_bgc">
       <img src={logo} alt="logo" class="Auth_Logo" />
       <span>Username</span>
-      <input class="Auth__input dark_green_border" value="" />
+      <input class="Auth__input dark_green_border" type="text"/>
       <span>Password</span>
-      <input class="Auth__input dark_green_border" value="" />
+      <input class="Auth__input dark_green_border" type="password" />
 
       <div class="Auth__buttons_container">
       <Link to="/dashboard"><button class="Auth__button_login lightest_green_bgc open-sans-bold">Login</button></Link>
