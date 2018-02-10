82506db4eb8bb83a1c95113303652f6d0ed00c92
Brett Hays <brett.g.hays.coding@gmail.com>
2018-02-10 08:11:06 -0700

    store and reducer done

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {HashRouter} from 'react-router-dom';
+import store from './store'
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
+, document.getElementById('root'));
+registerServiceWorker();
