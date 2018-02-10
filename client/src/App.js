d0e2a5f1f4d0ce57b915c0f22dc4052a1e78a88e
Pack <apack@UTMPG04820.local>
2018-02-10 10:33:39 -0700

    Login Screen changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Routes from './routes';
-import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        
-                   <div className='link-wrap'>
-
-                        <p><Link to='/' className='links'>Login Screen</Link></p>
-                        
-                        <p><Link to='/dashboard' className='links'>Dashboard</Link></p>
-
-                        {Routes}
-                   </div>  
+         <div>
+            {Routes}
+          </div>  
                    
         
     );
@@ -25,3 +17,4 @@ class App extends Component {
 
 let mapStateToProps = (state) => state;
 export default connect(mapStateToProps)(App);
+
