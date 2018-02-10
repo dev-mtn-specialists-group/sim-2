a0edb463ab5136327cc33ec975068aaaf211d085
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-02-10 11:25:00 -0700

    Merge branch 'master' of https://github.com/dev-mtn-specialists-group/sim-2 into wizard2

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from "../Navbar/Navbar";
 
 export default class Dashboard extends Component {
 
   render() {
     return (
-      <div>
+      <div id="root">
+        <Navbar />
         Dashboard
       </div>
     );
